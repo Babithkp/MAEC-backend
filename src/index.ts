c5a32d5a8268587e9fab9823983f745f366d68b0
Babithkp <babithkullachetti44@gmail.com>
2024-07-14 01:48:17 +0530

    env added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+import dotenv from "dotenv";
+dotenv.config();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
